bfb0c09d24e74ccd76b4625dc3603a81339ec64a
viniston <viniston.viniston@gmail.com>
2017-12-18 23:56:27 +0530

    EMRM gantt view

---
+++ assets/local-storage.js
@@ -0,0 +1,24 @@
+﻿window.rgStorage = {
+    set: function (key, value) {
+        try {
+            return window.localStorage.setItem(key, value);
+        } catch (ex) { }
+    },
+    get: function (key) {
+        try {
+            return window.localStorage.getItem(key);
+        } catch (ex) {
+            return null;
+        }
+    },
+    delete: function (key) {
+        try {
+            return window.localStorage.removeItem(key);
+        } catch (ex) {
+            return null;
+        }
+    },
+    has: function (key) {
+        return window.rgStorage.get(key) !== null;
+    }
+};
